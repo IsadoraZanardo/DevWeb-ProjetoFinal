a3d2deb72eafa4d1c61bf684707c980b23823a9c
Rayan Luca <rayanlucateixeira10@gmail.com>
2026-05-14 20:20:37 -0300

    aws questões

--- Frontend/DoUAUlingo/app/awschalenge/medio.js
+++ Frontend/DoUAUlingo/app/awschalenge/medio.js
@@ -410,23 +410,35 @@ export default function ChallengeScreen() {
       )}
 
       <TouchableOpacity
-        disabled={!allAnswered || submitted}
-        style={[
-          styles.button,
-          (!allAnswered || submitted) && styles.disabledButton,
-        ]}
-        onPress={submitChallenge}
-      >
-        <Text style={styles.buttonText}>
-          {submitted
-            ? "AVALIAÇÃO ENVIADA"
-            : allAnswered
-            ? "ENVIAR RESPOSTAS"
-            : "RESPONDA TODAS AS QUESTÕES"}
-        </Text>
-      </TouchableOpacity>
-    </ScrollView>
-  );
+  disabled={!allAnswered || submitted}
+  style={[
+    styles.button,
+    (!allAnswered || submitted) && styles.disabledButton,
+  ]}
+  onPress={submitChallenge}
+>
+  <Text style={styles.buttonText}>
+    {submitted
+      ? "AVALIAÇÃO ENVIADA"
+      : allAnswered
+      ? "ENVIAR RESPOSTAS"
+      : "RESPONDA TODAS AS QUESTÕES"}
+  </Text>
+</TouchableOpacity>
+
+{submitted && (
+  <TouchableOpacity
+    style={styles.backBottomButton}
+    onPress={() => router.replace("/(tabs)/dashboard")}
+  >
+    <Text style={styles.backBottomText}>
+      ← Voltar ao Dashboard
+    </Text>
+  </TouchableOpacity>
+)}
+
+</ScrollView>
+);
 }
 
 const styles = StyleSheet.create({
@@ -699,4 +711,19 @@ const styles = StyleSheet.create({
     fontWeight: "900",
     marginBottom: 20,
   },
+  backBottomButton: {
+  marginTop: 16,
+  backgroundColor: "#222",
+  paddingVertical: 16,
+  borderRadius: 18,
+  alignItems: "center",
+  borderBottomWidth: 5,
+  borderBottomColor: "#111",
+},
+
+backBottomText: {
+  color: "#fff",
+  fontSize: 14,
+  fontWeight: "900",
+},
 });
